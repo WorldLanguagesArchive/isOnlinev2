7f22b1b48186c451f3152a9acda57842d3c072e4
World_Languages <mundofinky@gmail.com>
2017-03-24 21:31:05 -0300

    Update .user.js

--- .user.js
+++ .user.js
@@ -8,46 +8,46 @@
 // @icon         https://raw.githubusercontent.com/WorldLanguages/isOnlinev2/master/logo.png
 // ==/UserScript==
 
-iOlog("Userscript started");
-var stop = 0;
-try {var localuser = Scratch.INIT_DATA.LOGGED_IN_USER.model.username;} catch(err) {var localuser = document.getElementsByClassName("profile-name")[0].innerHTML;}
+iOlog("isOnline started");
+stop = 0;
+try {localuser = document.documentElement.innerHTML.substring(document.documentElement.innerHTML.search("username")+10,document.documentElement.innerHTML.search("username")+100).match(/\'(.*)\'/).pop();xyz();}
+catch(err) {document.onreadystatechange = function(){if(document.readyState === 'complete');{localuser = document.getElementsByClassName("profile-name")[0].innerHTML;xyz();}}}
 
-if (window.location.href.startsWith("https://scratch.mit.edu/verify")) {
-    document.documentElement.innerHTML = "<center><h1 style='font-family:verdana';>Validating...</h1></center>";
-    var xmlhttp = new XMLHttpRequest();xmlhttp.open("GET", ' https://scratchtools.tk/isonline/api/v1/' + localuser + '/' + location.hash.substring(1) + "/test/", true);xmlhttp.send();
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState === 4 && xmlhttp.responseText.includes("true")) {
-            localStorage.setItem("iOuser", Scratch.INIT_DATA.LOGGED_IN_USER.model.username);
-            localStorage.setItem("iOkey", location.hash.substring(1));
-            document.documentElement.innerHTML = "<center><h1 style='font-family:verdana; color:green'>Successfully validated your Scratch account. isOnline is now working. <br>You can close this tab.</h1></center>";}
-        else {
-            document.documentElement.innerHTML = "<center><h1 style='font-family:verdana; color:red'>An error occurred. Please contact <a href='https://scratch.mit.edu/users/chooper100#comments'>@chooper100</a> if you come from isOnline account validation.</h1></center>";}}}
+function xyz() {
+
+    if (window.location.href.startsWith("https://scratch.mit.edu/verify")) {
+        stop = 1;
+        document.documentElement.innerHTML = "<center><h1 style='font-family:verdana';>Validating...</h1></center>";
+        xmlhttp = new XMLHttpRequest();xmlhttp.open("GET", ' https://scratchtools.tk/isonline/api/v1/' + localuser + '/' + location.hash.substring(1) + "/test/", true);xmlhttp.send();
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState === 4 && xmlhttp.responseText.includes("true")) {
+                localStorage.setItem("iOuser", localuser);
+                localStorage.setItem("iOkey", location.hash.substring(1));
+                document.documentElement.innerHTML = "<center><h1 style='font-family:verdana; color:green'>Successfully validated your Scratch account. isOnline is now working. <br>You can close this tab.</h1></center>";}
+            if (xmlhttp.readyState === 4 && xmlhttp.responseText.includes("true")===false) {
+                document.documentElement.innerHTML = "<center><h1 style='font-family:verdana; color:red'>An error occurred. Please contact <a href='https://scratch.mit.edu/users/chooper100#comments'>@chooper100</a> if you come from isOnline account validation.</h1></center>";}}}
 
 
-if (localStorage.getItem("iOuser") === null) {
-    stop = "User didn't validate any account. Stopped script."; try {
-        isError();
-        document.getElementsByClassName("confirm-email banner")[0].style.display = "block";document.getElementsByClassName("confirm-email banner")[0].style.color = "black";
-        document.getElementsByClassName("confirm-email banner")[0].innerHTML = "<span>Whoops! Looks like you didn't validate your account on isOnline. isOnline won't work until you <a href='https://scratchtools.tk/isonline/register' target='blank' >validate your account</a>.</span> It takes around 20 seconds.";} catch(err){}
-}
+    if (localStorage.getItem("iOuser") === null) {
+        stop = "User didn't validate any account. Stopped script."; try {
+            isError();
+            document.getElementsByClassName("confirm-email banner")[0].style.display = "block";document.getElementsByClassName("confirm-email banner")[0].style.color = "black";
+            document.getElementsByClassName("confirm-email banner")[0].innerHTML = "<span>Whoops! Looks like you didn't validate your account on isOnline. isOnline won't work until you <a href='https://scratchtools.tk/isonline/register' target='blank' >validate your account</a>.</span> It takes around 20 seconds.";} catch(err){}
+    }
 
-if (localuser != localStorage.getItem("iOuser") && window.location.href.includes("users") && localStorage.getItem("iOuser") !== null) {
-    stop = "User validated another account. Stopped script.";
+    if (localuser != localStorage.getItem("iOuser") && window.location.href.includes("users") && localStorage.getItem("iOuser") !== null) {
+        stop = "User validated another account. Stopped script.";
         isError();
         document.getElementsByClassName("confirm-email banner")[0].style.display = "block";document.getElementsByClassName("confirm-email banner")[0].style.color = "black";
         document.getElementsByClassName("confirm-email banner")[0].innerHTML = "<span>Whoops! Looks like you didn't validate isOnline on the account you are using now, so it's not working. If needed, login to " + localStorage.getItem("iOuser") + ", or unregister it by <a href='https://scratchtools.tk/isonline/register' target='blank'>registering " + localuser + " instead</a>.  <a onclick='document.getElementsByClassName(\"confirm-email banner\")[0].style.display = \"none\";'>X</span>";
-}
+    }
 
 
+    if(stop!==0){iOlog(stop);return;}
 
-if (stop === 0) {
-
-
-
-
-    var url = window.location.href;
-    var user = window.location.href.substring(30,100).substring(0, window.location.href.substring(30,100).indexOf('/'));
-    var key = localStorage.getItem("iOkey");
+    url = window.location.href;
+    user = window.location.href.substring(30,100).substring(0, window.location.href.substring(30,100).indexOf('/'));
+    key = localStorage.getItem("iOkey");
     iOlog("Profile: " + user);
     iOlog("Local username: " + localuser);
 
@@ -58,24 +58,24 @@ if (stop === 0) {
 
 
 
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.open("GET", 'https://scratchtools.tk/isonline/api/v1/' + localuser + '/' + key + '/set/online', true);
-        xmlhttp.send();
-        localStorage.setItem("iOlastOn", time());
-        iOlog("Sent online request");
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", 'https://scratchtools.tk/isonline/api/v1/' + localuser + '/' + key + '/set/online', true);
+    xmlhttp.send();
+    localStorage.setItem("iOlastOn", time());
+    iOlog("Sent online request");
 
 
-        if (url.substring(24,29) == 'users' && (url.match(/\//g) || []).length == 5 ) {
-            iOlog("Detected user is in a profile");
+    if (url.substring(24,29) == 'users' && (url.match(/\//g) || []).length == 5 ) {
+        iOlog("Detected user is in a profile");
 
-            document.getElementsByClassName("location")[0].innerHTML = document.getElementsByClassName("location")[0].innerHTML + ' | <p style="display:inline" id="iOstatus">Loading status...</p>';
+        document.getElementsByClassName("location")[0].innerHTML = document.getElementsByClassName("location")[0].innerHTML + ' | <p style="display:inline" id="iOstatus">Loading status...</p>';
 
-            if (localuser.toUpperCase() == user.toUpperCase()) {iOlog("Detected user is their own profile");isOnline();} else {status();}}
-
-
+        if (localuser.toUpperCase() == user.toUpperCase()) {iOlog("Detected user is their own profile");isOnline();} else {status();}}
 
 
-} else {iOlog(stop);}
+
+
+}
 
 
 
@@ -104,14 +104,14 @@ function status() {
 
     iOlog("Started status scan");
 
-    var xmlhttp = new XMLHttpRequest();xmlhttp.open("GET", ' https://scratchtools.tk/isonline/api/v1/' + localuser + '/' + key + "/get/" + user, true);xmlhttp.send();
+    xmlhttp = new XMLHttpRequest();xmlhttp.open("GET", ' https://scratchtools.tk/isonline/api/v1/' + localuser + '/' + key + "/get/" + user, true);xmlhttp.send();
 
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
 
             response  = xmlhttp.responseText;
-            var timestamp = JSON.parse(response).timestamp;
-            var status = JSON.parse(response).status;
+            timestamp = JSON.parse(response).timestamp;
+            status = JSON.parse(response).status;
 
             if (status == "online") {
                 if (time() - timestamp < 300) {isOnline();} else{isOffline();}}
@@ -145,7 +145,7 @@ function status() {
 
 function absent() {
     if (time()-localStorage.getItem("iOlastOn") > 240 && time()-localStorage.getItem("iOlastAbs") > 120 && stop == 0) {
-        var xmlhttp = new XMLHttpRequest();
+        xmlhttp = new XMLHttpRequest();
         xmlhttp.open("GET", 'https://scratchtools.tk/isonline/api/v1/' + localuser + '/' + key + '/set/absent', true);
         xmlhttp.send();
         localStorage.setItem("iOlastAbs", time());
@@ -174,6 +174,7 @@ function noiO() {
     document.getElementById("iOstatus").innerHTML = '<span title="This user has to install isOnline in order to show their status">Not iO user</span>';}
 
 function isError() {
+    console.log("error");
     try { document.getElementById("iOstatus").innerHTML = '<span title="Error getting the status. Read the orange box above">Error</span>';} catch(err){
         document.getElementsByClassName("location")[0].innerHTML = document.getElementsByClassName("location")[0].innerHTML + ' | <span title="Error getting the status. Read the orange box above">Error</span>';}}
 
@@ -188,3 +189,13 @@ function iOlog(x) {
 
 function time() {
     return Math.floor(Date.now() / 1000);}
+
+// Easter egg
+if (window.location.href.includes("users")) { try{
+String.prototype.replaceAll=function(a,b,c){return this.replace(new RegExp(a.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),c?"gi":"g"),"string"==typeof b?b.replace(/\$/g,"$$$$"):b);};
+window.addEventListener('load', function () {
+    var comment = document.getElementsByClassName("content"),emoji = "<img src='https://staging.scratchtools.tk/isonline/isonline-logo.png' height='20' width='20'></img>";
+    for(var i = 0; i < comment.length; i++){
+        comment[i].innerHTML = comment[i].innerHTML.replaceAll("_isOnline_", emoji).replaceAll("_isonline_", emoji);}
+});}catch(err){}}
+// Easter egg
