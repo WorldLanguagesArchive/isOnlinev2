a0402c50f03b607a00c9313c4df3786ca0662770
World_Languages <mundofinky@gmail.com>
2017-06-26 06:43:57 -0300

    Add files via upload

---
+++ background.js
@@ -0,0 +1,34 @@
+﻿chrome.runtime.onInstalled.addListener(function(details){
+    if(details.reason == "install"){
+        chrome.tabs.create({url:"https://scratchtools.tk/isonline/register/"});}
+    if(details.reason == "update"){
+        //chrome.tabs.create({url: "https://isonlineupdate.blogspot.com/"});
+		}
+});
+
+(function(){
+    chrome.tabs.query({url:"https://scratch.mit.edu/*"}, function(tabs) {
+        if (tabs.length===0){chrome.browserAction.setBadgeText({text: ""});}
+    });
+    setTimeout(arguments.callee, 10000);
+})();
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log(request);
+        if (request.status == "") {
+            chrome.browserAction.setBadgeText({text: ""});}
+        if (request.status == "orange") {
+            badge("#FFA500");}
+        if (request.status == "grey") {
+            badge("#808080");}
+        if (request.action == "reload") {
+            location.reload();}
+    });
+
+
+
+function badge(thecolor) {
+    chrome.browserAction.setBadgeText({text: " "});
+    chrome.browserAction.setBadgeBackgroundColor({color: thecolor});
+}
