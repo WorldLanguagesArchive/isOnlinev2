5ea8dd163ec4e450b417e033478c519f1a83215b
World_Languages <mundofinky@gmail.com>
2017-11-07 16:44:34 -0300

    Fix FAQ for firefox

--- extension/code.js
+++ extension/code.js
@@ -96,7 +96,7 @@ if(location.href.toLowerCase().startsWith("https://scratch.mit.edu/users/isonlin
             qEl.appendChild(div);
             faq.appendChild(qEl);
             qEl.addEventListener("click", function(e){
-                if(e.path[0].className === "faq-answer") return;
+                if(e.target.className === "faq-answer") return;
                 if(qSelected) qSelected.querySelector(".faq-answer").remove();
                 if(qSelected && qSelected === qEl) {
                     qSelected = null;
@@ -203,7 +203,6 @@ function main() {
     if ((time()-localStorage.getItem("iOlastOn") > 10 || time()-localStorage.getItem("iOlastOn") < -1) && localstatus() == "online") {setOnline();}
 
     /* Manage statuses */ update();setInterval(update, 3000);
-			console.log("Ran updateeventlistener");//ttt
 
     if (url.substring(24,29) == 'users' && (url.match(/\//g) || []).length == 5) {
         iOlog("Detected user is in a profile");
@@ -308,7 +307,6 @@ iONotify.setup();
 
 
 function update() {
-		console.log("Ran update");//ttt
     if (stop !== 0 || window.location.href.startsWith("https://scratch.mit.edu/isonline-extension")) {return;}
     if (localstatus() == "online") {
         updateStatus("");
@@ -492,7 +490,6 @@ function setOnline() {
     localStorage.setItem("iOlastOn", time());}
 
 function updateStatus(color) {
-	console.log("Ran updateStatus");//ttt
     chrome.runtime.sendMessage({color});
     try {document.getElementsByClassName("user-name dropdown-toggle")[0].style.backgroundColor=color;}
     catch(err) {try{document.getElementsByClassName("link right account-nav")[0].style.backgroundColor=color;}catch(err){}}
